5e50572a59ff12b5882488f75018b311fc62a9d7
Chad Niedert <>
2021-04-08 02:28:57 +0000

    reused nested for loops to created an array of all products

--- main.swift
+++ main.swift
@@ -4,43 +4,32 @@ var max = 0
 var prod = 0
 var ones, tens, hundreds, thousands, tenThousands, hundredThousands: Int
 
+var prods: [Int] = []
 
+// loop to build product array
 for i in 100...999 {
 	for j in 100...999 {
-		prod = i * j
+		prods.append(i * j)
+	}	
+}
 
-		// determine if number is 5 digits or 6 digits
-		if prod < 100_000 {
-			ones = prod % 10
-			prod /= 10
-			tens = prod % 10
-			prod /= 10
-			hundreds = prod % 10
-			prod /= 10
-			thousands = prod % 10
-			prod /= 10
-			tenThousands = prod
-			if ones == tenThousands && tens == thousands && prod > max {
-				max = prod
-				print("i * j = \(i) * \(j) = \(i * j)") 
-			}
-		} else {
-			ones = prod % 10
-			prod /= 10
-			tens = prod % 10
-			prod /= 10
-			hundreds = prod % 10
-			prod /= 10
-			thousands = prod % 10
-			prod /= 10
-			tenThousands = prod % 10
-			prod /= 10
-			hundredThousands = prod
-			if ones == hundredThousands && tens == tenThousands && hundreds == thousands && prod > max {
-				max = prod
-				print("i * j = \(i) * \(j) = \(i * j)") 
-			}
-		}
+let sortedDescending = Array(prods.sorted().reversed())
 
+for num in sortedDescending {
+	ones = prod % 10
+	prod /= 10
+	tens = prod % 10
+	prod /= 10
+	hundreds = prod % 10
+	prod /= 10
+	thousands = prod % 10
+	prod /= 10
+	tenThousands = prod % 10
+	prod /= 10
+	hundredThousands = prod
+
+	if ones == hundredThousands && tens == tenThousands && hundreds == thousands {
+		print(num)
+		break
 	}
 }
